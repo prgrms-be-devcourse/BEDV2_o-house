a69662b7d047572329d124ed9a1de29ddc0363a8
kkj0419 <kkhi0419@gmail.com>
2022-06-23 23:03:57 +0900

    [refactor] 필요없는 임포트 제거

--- src/test/java/com/prgrms/ohouse/domain/user/application/impl/UserServiceImplTest.java
+++ src/test/java/com/prgrms/ohouse/domain/user/application/impl/UserServiceImplTest.java
@@ -1,6 +1,5 @@
 package com.prgrms.ohouse.domain.user.application.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
@@ -9,12 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.prgrms.ohouse.domain.user.application.commands.UserCreateCommand;
 import com.prgrms.ohouse.domain.user.application.commands.UserLoginCommand;
-import com.prgrms.ohouse.domain.user.model.DuplicateEmailException;
 import com.prgrms.ohouse.domain.user.model.User;
 import com.prgrms.ohouse.domain.user.model.UserRepository;
 
@@ -37,7 +34,7 @@ class UserServiceImplTest {
 
 	@Test
 	@Transactional
-	void signUpTest() throws DuplicateEmailException {
+	void signUpTest() {
 		userService.signUp(createCommand);
 		User findUser = userRepository.findByEmail(createCommand.getEmail()).get();
 		assertThat(passwordEncoder.matches(createCommand.getPassword(), findUser.getPassword()), is(true));
@@ -45,7 +42,7 @@ class UserServiceImplTest {
 
 	@Test
 	@Transactional
-	void loginTest() throws DuplicateEmailException {
+	void loginTest(){
 		userService.signUp(createCommand);
 		UserLoginCommand command = new UserLoginCommand(createCommand.getEmail(), createCommand.getPassword());
 
