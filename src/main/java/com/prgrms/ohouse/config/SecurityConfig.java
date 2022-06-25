b0b48cc45fd823e0750cb161bac5669950a8a85b
kkj0419 <kkhi0419@gmail.com>
2022-06-25 15:21:00 +0900

    [refactor] security 설정 수정

--- src/main/java/com/prgrms/ohouse/config/SecurityConfig.java
+++ src/main/java/com/prgrms/ohouse/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.prgrms.ohouse.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -21,29 +22,29 @@ import com.prgrms.ohouse.domain.common.security.TokenProvideService;
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final TokenProvideService tokenProvideService;
-    private final JwtTokenProvider jwtTokenProvider;
+	private final TokenProvideService tokenProvideService;
+	private final JwtTokenProvider jwtTokenProvider;
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(tokenProvideService);
-    }
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(tokenProvideService);
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .antMatchers("/api/user/**").permitAll()
-                .and()
-                .addFilterAfter(new JwtAuthenticationFilter(jwtTokenProvider), SecurityContextPersistenceFilter.class)
-                ;
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.authorizeRequests()
+			.anyRequest().permitAll()
+			.and()
+			.addFilterAfter(new JwtAuthenticationFilter(jwtTokenProvider), SecurityContextPersistenceFilter.class)
+		;
+	}
 }
