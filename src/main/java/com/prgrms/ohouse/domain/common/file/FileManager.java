07687e417ff9d2f6eb3f8d503e27ce82c0699421
Hanbin Lee <logos1018@gmail.com>
2022-06-26 06:24:27 +0900

    [remove] 더미 데이터 삭제

--- src/main/java/com/prgrms/ohouse/domain/common/file/FileManager.java
+++ src/main/java/com/prgrms/ohouse/domain/common/file/FileManager.java
@@ -7,13 +7,6 @@ import org.springframework.web.multipart.MultipartFile;
 import com.prgrms.ohouse.domain.common.ImageAttachable;
 
 public interface FileManager {
-	/**
-	 *
-	 * @param multipartFiles
-	 * @param attached
-	 * @param <T>
-	 * @return
-	 */
 	<T extends ImageAttachable> List<StoredFile> store(List<MultipartFile> multipartFiles, T attached);
 
 	<T extends ImageAttachable> StoredFile store(MultipartFile multipartFile, T attached);
