2d71851e567d803bca80739d1f493e51bb9469ee
Minjae Lee <65841596+blessing333@users.noreply.github.com>
2022-06-28 17:01:28 +0900

    [fix] LocalFileUploader 파일 삭제 기능 누락 수정

--- src/test/java/com/prgrms/ohouse/domain/common/file/LocalFileUploadTest.java
+++ src/test/java/com/prgrms/ohouse/domain/common/file/LocalFileUploadTest.java
@@ -1,14 +1,13 @@
 package com.prgrms.ohouse.domain.common.file;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
@@ -16,13 +15,12 @@ import org.springframework.mock.web.MockMultipartFile;
 import com.prgrms.ohouse.infrastructure.file.LocalFileUploader;
 
 @SpringBootTest(properties = "spring.profiles.active:test")
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class LocalFileUploadTest {
 
 	@Autowired
 	private LocalFileUploader fileUploader;
 
-	@AfterAll
+	@AfterEach
 	private void deleteAllFile() throws IOException {
 		fileUploader.deleteAllFile();
 	}
@@ -36,9 +34,26 @@ class LocalFileUploadTest {
 			"image/png",
 			"<<png data>>".getBytes());
 
-		Assertions.assertDoesNotThrow(() -> {
+		assertDoesNotThrow(() -> {
 			String path = fileUploader.save(file, "testfile");
-			assertThat(path).isEqualTo("http://localhost:8080/testfile");
+			assertThat(path).isEqualTo("src/test/resources/static/testfile");
+			assertTrue(fileUploader.existByFileUrl(path));
 		});
+
+	}
+
+	@DisplayName("로컬서버 파일 삭제 테스트")
+	@Test
+	void testFileDelete() {
+		MockMultipartFile file = new MockMultipartFile(
+			"thumbnail",
+			"test.png",
+			"image/png",
+			"<<png data>>".getBytes());
+		String path = fileUploader.save(file, "testfile");
+
+		fileUploader.delete(path);
+
+		assertFalse(fileUploader.existByFileUrl(path));
 	}
 }
