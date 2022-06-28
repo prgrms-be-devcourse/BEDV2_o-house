d3045c035856d35770b5513aa31ed6bb64eb8d60
Minjae Lee <65841596+blessing333@users.noreply.github.com>
2022-06-28 13:50:41 +0900

    [fix] QuestionPostService.java 잘못된 import 제거

--- src/main/java/com/prgrms/ohouse/domain/community/model/post/question/QuestionPostService.java
+++ src/main/java/com/prgrms/ohouse/domain/community/model/post/question/QuestionPostService.java
@@ -3,7 +3,6 @@ package com.prgrms.ohouse.domain.community.model.post.question;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.prgrms.ohouse.domain.community.model.post.question.command.QuestionPostRegisterCommand;
-import com.prgrms.ohouse.domain.community.model.post.question.command.QuestionPostUpdateCommand;
 
 public interface QuestionPostService {
 	@Transactional
