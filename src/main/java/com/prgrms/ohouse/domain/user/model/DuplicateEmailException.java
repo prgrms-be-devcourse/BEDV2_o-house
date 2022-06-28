82e468c8cc9cd6afa0e95bcd56d1e91478fbba54
kkj0419 <kkhi0419@gmail.com>
2022-06-28 13:54:49 +0900

    [exception] 도메인 예외처리 추가

--- src/main/java/com/prgrms/ohouse/domain/user/model/DuplicateEmailException.java
+++ src/main/java/com/prgrms/ohouse/domain/user/model/DuplicateEmailException.java
@@ -2,7 +2,7 @@ package com.prgrms.ohouse.domain.user.model;
 
 public class DuplicateEmailException extends RuntimeException{
 
-	public DuplicateEmailException(String msg) {
-		super(msg);
+	public DuplicateEmailException(String message) {
+		super(message);
 	}
 }
---
+++ src/main/java/com/prgrms/ohouse/domain/user/model/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.prgrms.ohouse.domain.user.model;
+
+//TODO
+public class UserNotFoundException extends RuntimeException {
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+}
